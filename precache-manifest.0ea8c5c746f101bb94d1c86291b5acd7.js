fccc233d2a21e392f95da860fda006de2606a251
Parth Purvesh <parth29dec@gmail.com>
2020-02-28 16:16:38 +0530

    Updates

---
+++ precache-manifest.0ea8c5c746f101bb94d1c86291b5acd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42980f2b229b577e0a51f0442e596e09",
+    "url": "/sort/index.html"
+  },
+  {
+    "revision": "95712f803cc57ba91c7e",
+    "url": "/sort/static/css/main.72279705.chunk.css"
+  },
+  {
+    "revision": "158b3bbebfb97d8b5f96",
+    "url": "/sort/static/js/2.4d189d8b.chunk.js"
+  },
+  {
+    "revision": "779022913f67bd8202aa70fa2261f4c5",
+    "url": "/sort/static/js/2.4d189d8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95712f803cc57ba91c7e",
+    "url": "/sort/static/js/main.f3210e82.chunk.js"
+  },
+  {
+    "revision": "9ec57a7d437947936e70",
+    "url": "/sort/static/js/runtime-main.0f84caed.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sort/static/media/logo.5d5d9eef.svg"
+  }
+]);
